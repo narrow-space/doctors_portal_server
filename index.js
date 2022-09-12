44ec4effc3ac7e09cb3f7f0e5fdd34449541de59
narrow space <hasanimrantri@gmail.com>
2022-09-12 17:07:33 +0600

    sdddc

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require('cors');
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
